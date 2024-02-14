568be973593a5bb0ee74f23951e7d382297555fa
Daynis Rodriguez <daynis.rodriguez@gantabi.com>
2024-02-14 17:32:08 +0100

    Configuration refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { provideAuth, getAuth } from '@angular/fire/auth'
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app'
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,22 +9,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from 'src/environments/environment';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { InteceptorService } from './root-app/interceptor/inteceptor.service';
+import { RootMaterialModule } from './root-material/root-material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule, 
+    HttpClientModule,
+    RootMaterialModule,
     BrowserAnimationsModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: InteceptorService, multi:true} ],
-  bootstrap: [AppComponent]
+    { provide: HTTP_INTERCEPTORS, useClass: InteceptorService, multi: true },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
